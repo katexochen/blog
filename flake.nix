d7e004bc33d9820a0587a6e48f4761e8c404d91f
Paul Meyer <katexochen0@gmail.com>
2025-08-04 20:30:43 +0200

    publish workflow
    Signed-off-by: Paul Meyer <katexochen0@gmail.com>

--- flake.nix
+++ flake.nix
@@ -6,6 +6,19 @@
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in
     {
+      packages.x86_64-linux.default =
+        pkgs.runCommand "website"
+          {
+            buildInputs = [ pkgs.hugo ];
+            src = pkgs.lib.cleanSource ./.;
+            preferLocalBuild = true;
+          }
+          ''
+            TEMP_DIR=$(mktemp -d)
+            cp -r $src/* $TEMP_DIR
+            cd $TEMP_DIR
+            hugo --destination $out
+          '';
       devShells.x86_64-linux.default = pkgs.mkShell {
         packages = with pkgs; [ hugo ];
       };
